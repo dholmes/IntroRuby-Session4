dc3fe0e36a883a76b752ac01f49e09f74d87808c
Dan Holmes <daniel.holmes@gmail.com>
2013-05-13 16:38:05 -0500

    Ok, NOW I'm all synced up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,5 @@
 module ApplicationHelper
 	def active_page(path)
-<<<<<<< HEAD
 		%q( class="active" ).html_safe if current_page?(path)
-	end	
-=======
-		%q( class="active").html_safe if current_page?(path)
-	end
->>>>>>> jwillis3-session4/master
+    end
 end
