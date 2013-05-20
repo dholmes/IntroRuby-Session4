0f2d7902c93ae9e2554a661522f644d920b27cb2
Dan Holmes <daniel.holmes@gmail.com>
2013-05-20 17:21:58 -0500

    broken, but mine...need to pull in class changes and get to homework

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
+  	email = params[:session][:email].downcase
+  	password = params[:session][:password]
+
+  	user = User.find_by_email(email)
+  	if user && user.authenticate(password)
+  		flash[:success] = "WOOT!"
+  		sign_in user	
+  	else
+  		flash[:error] = "Awww...fail!"
+  		# set message and ...da
+  		# redirect them back to login
+  	end
   end
 
   def destroy
+  	sign_out
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+	 def sign_in(user)
+	 	session[:user_id] = @current_user.id
+	 	is_authenticated
+	 end
+	 def sign_out
+	 	self.current_user = nil
+	 	session[:user_id] = nil
+	 	reset_session
+	 end
+	 def current_user=(user)
+	 	@current_user = user
+	 end
+	 def get_authenticated_user
+	 	if session[:user_id] then 
+	 		self.current_user = User.find(session[:user_id])
+	 	
+	 	return self.current_user
+	 end
+
 end
